402ce9338f1d9719334afcd0c7fde1fa755747c9
Mierley <88342799+Mierley@users.noreply.github.com>
2021-08-04 19:28:23 +0300

    Create Salesman.cpp
    0

---
+++ Salesman.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	int n, INF = 0;
+	cin >> n;
+	vector < vector<int> > a(n, vector <int>(n, 0));
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			cin >> a[i][j];
+
+	vector < vector<int> > d((1 << n), vector <int>(n, INF));
+
+	d[0][0] = 0;
+
+	for (int mask = 0; mask < (1 << n); mask++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (d[mask][i] == INF)
+				continue;
+
+			for (int j = 0; j < n; j++)
+			{
+				if (!(mask & (1 << j)))
+					d[mask ^ (1 << j)][j] = min(d[mask ^ (1 << j)][j], d[mask][i] + a[i][j]);
+			}
+		}
+
+	}
+	cout << d[(1 << n) - 1][0] << endl;
+}
+
